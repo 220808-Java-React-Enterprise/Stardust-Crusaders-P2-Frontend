431be655738578656baf89584a331da568b6dde2
Toddkirby <todd260@revature.net>
2022-09-14 17:19:46 -0400

    attempting to merge

---
+++ pokecare/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Signup from './components/signup/signup';
+
+function App() {
+  return (
+    <Signup />
+  );
+}
+
+export default App;
