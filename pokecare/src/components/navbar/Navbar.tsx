957d3543b7a76916edce1da35bb024222b4951c9
mlopez44 <60417513+mlopez44@users.noreply.github.com>
2022-09-14 16:04:07 -0400

    It's working!

---
+++ pokecare/src/components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+
+import { useNavigate } from "react-router-dom";
+import "./Navbar.css";
+
+export default function Navbar() {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <body>
+            <div className="topnav">
+    <a href="#" id="test" onClick={() => navigate("/home")}><img alt="Its broken!" src="assets\lancer.png"width="100%" height="100%"></img></a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a className="cta" onClick={() => navigate("/signup")}>Sign In</a>
+</div>
+
+            </body>
+        </>
+    );
+}
