5af5654538d998a01f610c06d55b494551cda8e0
Toddkirby <todd260@revature.net>
2022-09-19 18:04:52 -0400

    to merge, both work

--- pokecare/src/utils/ApiConfigs.ts
+++ pokecare/src/utils/ApiConfigs.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const PokeApi = axios.create({
-    //http://whatverIpUrl:8080/pokecare
     baseURL: "http://pokecare-env.eba-vvzgvtim.us-east-1.elasticbeanstalk.com/pokecare/",
     headers: {
         "Content-type": "application/json"
