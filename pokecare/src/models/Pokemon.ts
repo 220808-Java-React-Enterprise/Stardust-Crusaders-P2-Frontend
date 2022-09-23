fe564670a7258a75bcfbd64c28a628e869728a9d
Toddkirby <todd260@revature.net>
2022-09-23 12:31:10 -0400

    pulls individual pokemon based on user input from api

--- pokecare/src/models/Pokemon.ts
+++ pokecare/src/models/Pokemon.ts
@@ -4,6 +4,7 @@ export default class Pokemon{
     name: string;
     level: number;
     xp_needed: number; 
+    abilities!: string[];
     ability: string;
     nature: string;
     pokedex_id: string;
@@ -12,12 +13,13 @@ export default class Pokemon{
 
     
 
-    constructor(pokemon_id: string, name: string, level: number, xp_needed: number, ability: string, nature: string,pokedex_id: string, /*daycare_id: string,*/ id: string ){
+    constructor(pokemon_id: string, name: string, level: number, xp_needed: number, abilities: string[], ability: string, nature: string, pokedex_id: string, /*daycare_id: string,*/ id: string ){
         this.pokemon_id = pokemon_id;
         this.name = name;
         this.level = level;
         this.xp_needed = xp_needed;
         this.ability = ability;
+        this.abilities = abilities;
         this.nature = nature;
         this.pokedex_id = pokedex_id;
         // this.daycare_id = daycare_id;
